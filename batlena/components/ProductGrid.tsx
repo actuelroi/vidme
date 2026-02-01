9d0cc59da084808f759845eee373db8fe0826a92
actuelroi <yebouareel@gmail.com>
2026-02-01 12:18:41 +0100

    space

--- batlena/components/ProductGrid.tsx
+++ batlena/components/ProductGrid.tsx
@@ -14,7 +14,7 @@ const ProductGrid = async () => {
 
      <Suspense fallback={<Loading/>}>
     
-    <div className='grid grid-cols-1 sm:grid-cols-2 p-4 items-center m-3 gap-2 md:grid-cols-5 md:p-4 md:gap-4'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 p-4 items-center m-3 gap-2 space-x-3 md:grid-cols-5 md:p-4 md:gap-4'>
     {
       data?.length > 0  && (
         data.map((product)=>(
